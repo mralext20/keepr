1a7a944c50a6157f5a8356886a539543edc17f6e
Alexander Terry <alexterry43@gmail.com>
2020-04-03 13:36:57 -0600

    add client id

--- wwwsrc/src/authConfig.js
+++ wwwsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "";
-export const audience = "";
-export const clientId = "";
+export const domain = "dev-8acee36z.auth0.com";
+export const audience = "https://my-app.com";
+export const clientId = "nxXfQSnGMxWtNLAdS3DCMsZey4SFH1QR";
