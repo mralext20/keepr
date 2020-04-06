be7ab21becf3d1c6e0d54ff3c47318b5edd6521a
Alexander Terry <alexterry43@gmail.com>
2020-04-06 14:53:56 -0600

    wireup vault keeps controller -> service -> repo

---
+++ Controllers/VaultKeepsController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Keepr.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  [Authorize]
+  public class VaultKeepsController : ControllerBase
+  {
+    private readonly VaultKeepsService _vks;
+
+    public VaultKeepsController(VaultKeepsService vks)
+    {
+      _vks = vks;
+    }
+  }
+}
